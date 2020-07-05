d8006792265af345efb3ef01f8eb35a5bfa124b6
DenireS <65292302+DenireS@users.noreply.github.com>
2020-07-05 16:17:02 +0300

    Updates

---
+++ precache-manifest.38ef345f1752b14ddc2e66c3d0e641f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87a8d3e7531ec7c7fd868e6093be07d",
+    "url": "/demo-denires-sn/index.html"
+  },
+  {
+    "revision": "55059c4cd880439bf3fd",
+    "url": "/demo-denires-sn/static/css/3.fe1ac95f.chunk.css"
+  },
+  {
+    "revision": "feacc3b55995a24744d1",
+    "url": "/demo-denires-sn/static/css/4.7bed7d8d.chunk.css"
+  },
+  {
+    "revision": "1a3776ca5761cf6cc921",
+    "url": "/demo-denires-sn/static/css/main.3744c8ea.chunk.css"
+  },
+  {
+    "revision": "dd799a3519dc08c71ed2",
+    "url": "/demo-denires-sn/static/js/2.3af1219d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-denires-sn/static/js/2.3af1219d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55059c4cd880439bf3fd",
+    "url": "/demo-denires-sn/static/js/3.850bb883.chunk.js"
+  },
+  {
+    "revision": "feacc3b55995a24744d1",
+    "url": "/demo-denires-sn/static/js/4.c5e5b867.chunk.js"
+  },
+  {
+    "revision": "1a3776ca5761cf6cc921",
+    "url": "/demo-denires-sn/static/js/main.00a0c2d0.chunk.js"
+  },
+  {
+    "revision": "81c0ef91201841267b3a",
+    "url": "/demo-denires-sn/static/js/runtime-main.73701fe1.js"
+  },
+  {
+    "revision": "a4c1f6995fe7851f5b06f4a770ebc318",
+    "url": "/demo-denires-sn/static/media/User.a4c1f699.jpg"
+  }
+]);
